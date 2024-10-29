4c90111f0624e1f4156196043c8543ccc6323112
juansezapiola <jzapiola@udesa.edu.ar>
2024-10-29 18:07:34 -0300

    tp3

---
+++ TPs/TP3/TP3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+---------- Análisis Descriptivo y Predicción de Desocupación ------------------
+
+--    Machine Learning para Economistas - Universidad de San Andrés          --
+
+@authores: Eitan Salischiker, Tomás Marotta y Juan Segundo Zapiola 
+
+Fecha: 15 de Noviembre de 2024
+
+"""
+
+
+import os
+
+os.chdir('/Users/juansegundozapiola/Documents/Maestria/Big Data/BigData/TPs/TP3')
+
+
+'''
+Parte I: Analizando la base
+'''
+
+#Inciso 1 (ver documento)
+
+#Inciso 2
+
+
+
+
+
